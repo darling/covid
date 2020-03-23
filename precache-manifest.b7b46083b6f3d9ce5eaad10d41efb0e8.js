4fda33ac95b8141da2a0ac2320406a028a3de1dc
Carter Black <carterb@mailbox.org>
2020-03-23 14:57:33 -0700

    UwU

---
+++ precache-manifest.b7b46083b6f3d9ce5eaad10d41efb0e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1e1489828782e739551048fe27f273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d740e030ed87b45b1fb",
+    "url": "/static/css/2.a6d3956a.chunk.css"
+  },
+  {
+    "revision": "5d740e030ed87b45b1fb",
+    "url": "/static/js/2.13874c3c.chunk.js"
+  },
+  {
+    "revision": "a499f0653a77f2af3e04",
+    "url": "/static/js/main.832ac053.chunk.js"
+  },
+  {
+    "revision": "e96a0da74f90d5bb9474",
+    "url": "/static/js/runtime-main.9009d273.js"
+  },
+  {
+    "revision": "812218b9f511ec5f5048e09ff205ee15",
+    "url": "/static/media/Coronavirus.812218b9.png"
+  },
+  {
+    "revision": "6ff91faaf7d45866c047daa92d43752d",
+    "url": "/static/media/bad-breath.6ff91faa.png"
+  },
+  {
+    "revision": "9f4fc680d39d0f495788367b5901efdd",
+    "url": "/static/media/cough-vector.9f4fc680.png"
+  },
+  {
+    "revision": "87d3bfe034bfdc3f11c8733e7dd769d8",
+    "url": "/static/media/fever.87d3bfe0.png"
+  },
+  {
+    "revision": "ddab595aad4963207bb21f173836ad5c",
+    "url": "/static/media/graphic-covid19.ddab595a.png"
+  },
+  {
+    "revision": "2f8d7eb2cd7b974490cf84bfcf332ca9",
+    "url": "/static/media/patient.2f8d7eb2.png"
+  }
+]);
